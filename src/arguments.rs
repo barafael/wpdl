f57d95638dc528a372e680fa49e93e8f33a0afb4
Rafael Bachmann <rafael.bachmann.93@gmail.com>
2023-01-21 19:00:26 +0100

    Use clap 4 style attribute names

--- src/arguments.rs
+++ src/arguments.rs
@@ -1,15 +1,15 @@
 #[derive(clap::Parser)]
-#[clap(version, author, about = "Downloads a webpage")]
+#[command(version, author, about = "Downloads a webpage")]
 pub struct Arguments {
     /// The URL of the webpage to download
-    #[clap(required = true)]
+    #[arg(required = true)]
     pub url: String,
 
     /// File to save the downloaded webpage to
-    #[clap(short, long)]
+    #[arg(short, long)]
     pub output: Option<String>,
 
     /// Activate syntax highlighting
-    #[clap(short, long, default_value_t = false)]
+    #[arg(short, long, default_value_t = false)]
     pub prettify: bool,
 }
